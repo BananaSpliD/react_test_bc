93ced6a2679a5c268cae6561f1335d5dd0d73c07
Bo Chen <bochen47@gmail.com>
2021-10-06 15:37:59 +0200

    Corrección de errores y añadido navbar

---
+++ src/components/Navigationbar.js
@@ -0,0 +1,18 @@
+import {Component} from 'react'
+import Navbar from 'react-bootstrap/Navbar';
+import Container from 'react-bootstrap/Container';
+import './Navbar.css';
+class Navigationbar extends Component{
+    render(){
+        return(
+            <Navbar bg="dark">
+                <Container>
+                    <img alt="bestcycling" height="60" src="https://www.cicloindoor.com/themes/bestcycling.es/images/logo-big-black.png"/>
+                </Container>
+                
+            </Navbar>
+        );
+    }
+}
+
+export default Navigationbar;
